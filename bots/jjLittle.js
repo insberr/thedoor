da4c4a50a0484ba01e4ccf2dc53b72eadab865c0
insberr <jonahmatteson@gmail.com>
2022-03-09 16:29:12 -0800

    added function

--- bots/jjLittle.js
+++ bots/jjLittle.js
@@ -4,12 +4,18 @@ module.exports = {
     name: "JJ's Little Friend",
     codename: "jjLittle",
     ignore: true,
+    commands: [
+        // TODO Add permissions so random users dont mess with the config lol **************************** plss
+        new SlashCommandBuilder()
+            .setName('chance')
+            .setDescription('The chance of the bot replying with "ur mom"')
+            .addNumberOption(option => option.setName('edit').setDescription('Edit the chance of the bot replying with "ur mom"').setMinValue(0.00001).setMaxValue(100)),
+
+        // blacklist channels
+    ],
     run(mgr) {
         const client = new Client({
-            intents: [
-                Intents.FLAGS.GUILDS,
-                Intents.FLAGS.GUILD_MESSAGES
-            ],
+            intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
         });
 
         client.once("ready", () => {
@@ -17,9 +23,30 @@ module.exports = {
         });
 
         client.addListener("messageCreate", (message) => {
-            console.log(message.content);
+            if (message.author.bot) return;
+
+            if (message.content.match(/i'?.*(to)? (pee|piss)( |$)/gim)?.length > 0) {
+                // Do stats stuff later *****************
+                // config.stats.iHaveToPee++;
+                // updateConfig();
+                if (Math.random < 1 / 100) {
+                    return message.reply("https://i.imgur.com/XqQZQ.gif");
+                }
+                return message.reply("go piss gowrl");
+            }
+
+            if (message.content.match(/ +L$| +L +|^ *L+ *$/gim)?.length > 0) {
+                message.react("🇱");
+                return;
+            }
+
+            let number = Math.random();
+            if (number < mgr.config.chance[this.codename] / 100) {
+                message.reply("L");
+                return;
+            }
         });
 
         client.login(mgr.config.tokens[this.codename]);
-    }
-}
+    },
+};
--- bots/theaBestie.js
+++ bots/theaBestie.js
@@ -4,6 +4,15 @@ module.exports = {
     name: "Thea's Bestie",
     codename: "theaBestie",
     ignore: false,
+    commands: [
+        // TODO Add permissions so random users dont mess with the config lol **************************** plss
+        new SlashCommandBuilder()
+            .setName('chance')
+            .setDescription('The chance of the bot replying with the dancing gif')
+            .addNumberOption(option => option.setName('edit').setDescription('Edit the chance of the bot replying with the dancing gif').setMinValue(0.00001).setMaxValue(100)),
+
+        // blacklist channels
+    ],
     run(mgr) {
         const client = new Client({
             intents: [
@@ -17,7 +26,12 @@ module.exports = {
         });
 
         client.addListener("messageCreate", (message) => {
-            // console.log(message.content);
+            if (message.author.bot) return;
+
+
+            if (number < mgr.config.chance[this.codename] / 100) {
+                message.reply("https://tenor.com/view/dance-kid-club-gif-9152583");
+            }
         });
 
         client.login(mgr.config.tokens[this.codename]);
